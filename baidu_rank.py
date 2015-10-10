e5d478a54052801e5ccd86456af38a4f68b22e67
jiangyoucai <150haomengting@163.com>
2015-10-11 00:00:40 +0800

    增加多域名对多关键词查询，增加注释，改进算法,修复bug 	修改：      baidu_rank.py

--- baidu_rank.py
+++ baidu_rank.py
@@ -1,4 +1,4 @@
-#coding:utf-8
+# coding:utf-8
 import sys
 import requests
 from pyquery import PyQuery
@@ -6,24 +6,48 @@ from pyquery import PyQuery
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-def get_rank(keyword,domain):
-    url='http://www.baidu.com/s?wd=%s'%keyword
-    doc=PyQuery(requests.get(url).text)
-    rank=0
-    for i,d in enumerate(doc('.c-container')):
-        d=PyQuery(d)
-        result='%s%s'%(d('.f13 .g'),d('.c-showurl'))
-        host=PyQuery(result)('span').text()
-        if domain in str(host):
-            rank=i+1
+
+def get_rank(keyword, domains):
+    # 拼接url
+    url = 'http://www.baidu.com/s?wd=%s&rn=50' % keyword
+    # 格式化
+    doc = PyQuery(requests.get(url).text)
+    # 存储排名结果，key：网址，value：排名
+    result = {}
+    # 循环父标签
+    for i, d in enumerate(doc('.c-container')):
+        # 格式化子标签
+        d = PyQuery(d)
+        # 获取网址字符串，非百度网址：.f13 .g，百度网址：.c-showurl
+        host = '%s%s' % (d('.f13 .g'), d('.c-showurl'))
+        # 如果不为空，获取网址内容，切割为域名形式，空值结果为百度新闻，跳过
+        if host != '':
+            host = PyQuery(host)('span').text().split('/')[0].split(' ')[0].split('...')[0]
+            # 存储key，value
+            result[host] = i + 1
+    # 存储排名返回值
+    rank = ''
+    # 循环域名
+    for domain in domains:
+        # 字典key值查询，如果有排名，则结果为value值，没有，则结果为0
+        if result.has_key(domain):
+            rank += ' %s' % result[domain]
+        else:
+            rank += ' 0'
     return rank
 
+
 def main():
-    keywords=['上海旅游']
-    domain='mafengwo.cn'
+    # 关键词
+    keywords = ['千岛湖', '东方明珠']
+    # 域名
+    domains = ['ticket.lvmama.com', 'menpiao.tuniu.com', 'piao.ctrip.com', 'www.ly.com']
+    # 循环
     for keyword in keywords:
-        rank=get_rank(keyword,domain)
-        print keyword,rank
-    
+        # 查排名
+        rank = get_rank(keyword, domains)
+        print keyword, rank
+
+
 if __name__ == '__main__':
     main()
